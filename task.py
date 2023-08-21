815c45bdf349ec2ffcbff82992196e2cf32b8a30
vanshhgupta <142612597+vanshhgupta@users.noreply.github.com>
2023-08-22 00:54:00 +0530

    Update task.py

--- task.py
+++ task.py
@@ -4,63 +4,42 @@ import numpy as np
 print("ENTER ANY KEY TO SEE NEXT IMAGE")
 #output 1
 
+def colourchange(img):
+    imgHSV= cv2.cvtColor(img,cv2.COLOR_BGR2Lab)
+    cv2.imshow("original",img)
+    cv2.imshow("output", imgHSV)
+    cv2.waitKey(0)
+
 img1=cv2.imread('1.png')
-imgHSV= cv2.cvtColor(img1,cv2.COLOR_BGR2Lab)
-cv2.imshow("original",img1)
-cv2.imshow("output", imgHSV)
-cv2.waitKey(0)
+colourchange(img1)
 
 img2=cv2.imread('2.png')
-imgHSV2= cv2.cvtColor(img2,cv2.COLOR_BGR2Lab)
-cv2.imshow("original",img2)
-cv2.imshow("output", imgHSV2)
-cv2.waitKey(0)
+colourchange(img2)
 
 img3=cv2.imread('3.png')
-imgHSV3= cv2.cvtColor(img3,cv2.COLOR_BGR2Lab)
-cv2.imshow("original",img3)
-cv2.imshow("output", imgHSV3)
-cv2.waitKey(0)
+colourchange(img3)
 
 img4=cv2.imread('4.png')
-imgHSV4= cv2.cvtColor(img4,cv2.COLOR_BGR2Lab)
-cv2.imshow("original",img4)
-cv2.imshow("output", imgHSV4)
-cv2.waitKey(0)
+colourchange(img4)
 
 img5=cv2.imread('5.png')
-imgHSV5= cv2.cvtColor(img5,cv2.COLOR_BGR2Lab)
-cv2.imshow("original",img5)
-cv2.imshow("output", imgHSV5)
-cv2.waitKey(0)
+colourchange(img5)
 
 img6=cv2.imread('6.png')
-imgHSV6= cv2.cvtColor(img6,cv2.COLOR_BGR2Lab)
-cv2.imshow("original",img6)
-cv2.imshow("output", imgHSV6)
-cv2.waitKey(0)
+colourchange(img6)
 
 img7=cv2.imread('7.png')
-imgHSV7= cv2.cvtColor(img7,cv2.COLOR_BGR2Lab)
-cv2.imshow("original",img7)
-cv2.imshow("output", imgHSV7)
-cv2.waitKey(0)
+colourchange(img7)
 
 img8=cv2.imread('8.png')
-imgHSV8= cv2.cvtColor(img8,cv2.COLOR_BGR2Lab)
-cv2.imshow("original",img8)
-cv2.imshow("output", imgHSV8)
-cv2.waitKey(0)
+colourchange(img8)
 
 img9=cv2.imread('9.png')
-imgHSV9= cv2.cvtColor(img9,cv2.COLOR_BGR2Lab)
-cv2.imshow("original",img9)
-cv2.imshow("output", imgHSV9)
-cv2.waitKey(0)
+colourchange(img9)
 
 img10=cv2.imread('10.png')
-imgHSV10= cv2.cvtColor(img10,cv2.COLOR_BGR2Lab)
-cv2.imshow("original",img10)
-cv2.imshow("output", imgHSV10)
-cv2.waitKey(0)
+colourchange(img10)
 
+#output3
+myColors = [[1,179,0,255,0,255],
+            [60,179,0,255,0,255]]
