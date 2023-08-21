5d289964c5be754fc01aadbd3c19d2b511558271
vanshhgupta <142612597+vanshhgupta@users.noreply.github.com>
2023-08-21 23:57:15 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+
+print("ENTER ANY KEY TO SEE NEXT IMAGE")
+#output 1
+
+img1=cv2.imread('1.png')
+imgHSV= cv2.cvtColor(img1,cv2.COLOR_BGR2Lab)
+cv2.imshow("original",img1)
+cv2.imshow("output", imgHSV)
+cv2.waitKey(0)
+
+img2=cv2.imread('2.png')
+imgHSV2= cv2.cvtColor(img2,cv2.COLOR_BGR2Lab)
+cv2.imshow("original",img2)
+cv2.imshow("output", imgHSV2)
+cv2.waitKey(0)
+
+img3=cv2.imread('3.png')
+imgHSV3= cv2.cvtColor(img3,cv2.COLOR_BGR2Lab)
+cv2.imshow("original",img3)
+cv2.imshow("output", imgHSV3)
+cv2.waitKey(0)
+
+img4=cv2.imread('4.png')
+imgHSV4= cv2.cvtColor(img4,cv2.COLOR_BGR2Lab)
+cv2.imshow("original",img4)
+cv2.imshow("output", imgHSV4)
+cv2.waitKey(0)
+
+img5=cv2.imread('5.png')
+imgHSV5= cv2.cvtColor(img5,cv2.COLOR_BGR2Lab)
+cv2.imshow("original",img5)
+cv2.imshow("output", imgHSV5)
+cv2.waitKey(0)
+
+img6=cv2.imread('6.png')
+imgHSV6= cv2.cvtColor(img6,cv2.COLOR_BGR2Lab)
+cv2.imshow("original",img6)
+cv2.imshow("output", imgHSV6)
+cv2.waitKey(0)
+
+img7=cv2.imread('7.png')
+imgHSV7= cv2.cvtColor(img7,cv2.COLOR_BGR2Lab)
+cv2.imshow("original",img7)
+cv2.imshow("output", imgHSV7)
+cv2.waitKey(0)
+
+img8=cv2.imread('8.png')
+imgHSV8= cv2.cvtColor(img8,cv2.COLOR_BGR2Lab)
+cv2.imshow("original",img8)
+cv2.imshow("output", imgHSV8)
+cv2.waitKey(0)
+
+img9=cv2.imread('9.png')
+imgHSV9= cv2.cvtColor(img9,cv2.COLOR_BGR2Lab)
+cv2.imshow("original",img9)
+cv2.imshow("output", imgHSV9)
+cv2.waitKey(0)
+
+img10=cv2.imread('10.png')
+imgHSV10= cv2.cvtColor(img10,cv2.COLOR_BGR2Lab)
+cv2.imshow("original",img10)
+cv2.imshow("output", imgHSV10)
+cv2.waitKey(0)
+
